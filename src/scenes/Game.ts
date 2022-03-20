68d2903c135b31a2313821121df7a13df9460c23
Under <jorgeolmo.I@gmail.com>
2022-03-20 18:49:08 +0100

    Setteo del personaje

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,7 +7,15 @@ export default class Game extends Phaser.Scene {
     }
 
     preload () {
+        // Carga del fondo
         this.load.image('background', 'house/bg_repeat_340x640.png')
+
+        // Carga del personaje
+        this.load.atlas(
+            'rocket-mouse',
+            'characters/rocket-mouse.png',
+            'characters/rocket-mouse.json'
+        )
     }
 
     create () {
@@ -16,9 +24,19 @@ export default class Game extends Phaser.Scene {
         const width = this.scale.width
         const height = this.scale.height
 
+        
         //this.add.image(0, 0, 'background').setOrigin(0, 0)
         // Estructura para poner una imagen que repita de manera automatica el tamaño de la ventana
         this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0)
+        
+        
+        // Creacion del personaje
+        this.add.sprite(
+            width * 0.5,
+            height * 0.5,
+            'rocket-mouse', // Key del atlas declarado mas arriba
+            'rocketmouse_fly01.png'
+        )
     }
 
 }
